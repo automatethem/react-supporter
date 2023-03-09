ab95e45dbddcf7edfa15109dc30fc9e46a765c6b
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-09 21:55:36 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import JumoButton from './components/JumoButton/JumoButton';
+
+const App = () => (
+  <JumoButton backgroundColor="blue" fontColor="white">
+    테스트 버튼
+  </JumoButton>
+);
+
+export default App;
