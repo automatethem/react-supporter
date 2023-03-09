a598cb906eed66daf5c6dc233c053195410331c5
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-09 22:19:16 +0900

    Create App.js

---
+++ examples/app/app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { JumoButton } from 'react-helper';
+
+const App = () => (
+  <JumoButton backgroundColor="green" fontColor="white">
+    테스트 버튼
+  </JumoButton>
+);
+
+export default App;
