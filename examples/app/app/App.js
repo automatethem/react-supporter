3a62dcc6e47b0163b1e9cb317c77acfbba10bed4
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-10 00:45:35 +0900

    Update App.js

--- examples/app/app/App.js
+++ examples/app/app/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import JumoButton from 'react-supporter';
 
 const App = () => (
-  <JumoButton backgroundColor="green" fontColor="white">
+  <JumoButton backgroundColor="green" fontColor="white" onClick={() => {console.log('test')}}>
     테스트 버튼
   </JumoButton>
 );
