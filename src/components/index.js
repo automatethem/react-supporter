363e18946b27ebdf007d9893a8d2dbc3d3b9c8d0
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-11 12:32:29 +0900

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,10 @@ import JumoButton from './JumoButton/JumoButton';
 export { JumoButton };
 */
 ///*
+#https://github.com/omgovich/react-colorful/blob/master/src/index.ts\
+export { JumoButton } from './JumoButton/JumoButton';
+//*/
+/*
 //https://talkwithcode.tistory.com/11
 export { default as JumoButton } from './JumoButton/JumoButton';
-//*/
+*/
