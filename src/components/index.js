748b345d83fbaddafb24191204db4236cc44664b
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-11 11:25:45 +0900

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,10 @@
+/*
+//https://tecoble.techcourse.co.kr/post/2021-07-13-react-component-npm-publishing/
 import JumoButton from './JumoButton/JumoButton';
 
 export { JumoButton };
+*/
+///*
+//https://talkwithcode.tistory.com/11
+export { default as JumoButton } from './JumoButton/JumoButton';
+//*/
